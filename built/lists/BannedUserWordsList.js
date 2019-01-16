7db2c7ee1d5bc88337d3a2781a161b30821ae630
unknown <polleps@gmail.com>
2019-01-16 19:19:03 +0100

    cleaning up ignored files

--- built/lists/BannedUserWordsList.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-class BannedUserWordsList extends _1.List {
-    get data() {
-        return this._list;
-    }
-}
-exports.BannedUserWordsList = BannedUserWordsList;
--- built/web/routers/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User_router_1 = require("./User.router");
-exports.UserRouter = User_router_1.UserRouter;
