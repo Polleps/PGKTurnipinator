ea334a846d41abbd8c383a66e64449585d11ae67
Polle Pas <polle.pas@rysst.nl>
2019-11-18 10:02:50 +0100

    fix error when no tournament doesnt have an image

--- agenda/src/image.js
+++ agenda/src/image.js
@@ -1,6 +1,11 @@
 import { html } from 'lit-html';
-
+import noImage from './assets/no-image.jpg'
 export const pf = (url) => {
+  const convertedURL = url ? replaceURL(url) : noImage;
+  return html`<div class="pf-image-holder"><img src=${convertedURL} alt="" loading="lazy"></div>`;
+}
+
+const replaceURL = (url) => {
   const crop = '?auto=compress,format&w=280&h=280&fit=crop';
-  return html`<div class="pf-image-holder"><img src=${url.replace('images.smash.gg', 'smashgg.imgix.net') + crop} alt="" loading="lazy"></div>`;
+  return url.replace('images.smash.gg', 'smashgg.imgix.net') + crop;
 }
