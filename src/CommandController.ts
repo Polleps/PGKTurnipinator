8aae689f69354503d80907511f3bdece74fb2c1c
BuildTools <polleps@gmail.com>
2019-12-30 21:08:31 +0100

    fixes issue where git add . is recognised as a command

--- src/CommandController.ts
+++ src/CommandController.ts
@@ -5,6 +5,9 @@ import Config from "./Config";
 import Help from "./Help";
 import { BasicSetCache } from "./stores";
 import { store } from "./Store";
+import { createLogger } from "./utils/logger";
+
+const log = createLogger("CommandController");
 
 export default class CommandController {
   private _commandFactory: CommandFactory;
@@ -25,37 +28,40 @@ export default class CommandController {
   }
 
   public processMessage(message: Discord.Message): void {
-    const fullSplit = message.content.split(" ");
-    if (fullSplit[0].length > 1 && fullSplit[0].startsWith(Config.CommandPrefix)) {
-      const commandMessage = message.content.substring(1);
-      const messageArr = commandMessage.split(" ");
-      const commandTag = messageArr.shift();
+    const commandRegex = new RegExp(`^\\${Config.CommandPrefix}{1}\\w{1,}`, "g");
 
-      if (commandTag.toLowerCase() === "help") {
-        message.reply(this._help.getHelp(message.author, messageArr));
-        message.delete();
+    if (!commandRegex.test(message.content)) {
+      return;
+    }
+
+    const commandMessage = message.content.substring(1);
+    const messageArr = commandMessage.split(" ");
+    const commandTag = messageArr.shift();
+
+    if (commandTag.toLowerCase() === "help") {
+      message.reply(this._help.getHelp(message.author, messageArr));
+      message.delete();
+      return;
+    }
+
+    if (this._commands.has(commandTag)) {
+      const command = this._commands.get(commandTag);
+      // Admin commands should only be able to be used by botadmins.
+      if (command.isAdminCommand && !this.isBotAdmin(message.author)) {
+        message.reply("This command can only be used by the staff.");
+        message.delete().catch((err) => log(err));
         return;
       }
-
-      if (this._commands.has(commandTag)) {
-        const command = this._commands.get(commandTag);
-        // Admin commands should only be able to be used by botadmins.
-        if (command.isAdminCommand && !this.isBotAdmin(message.author)) {
-          message.reply("This command can only be used by the staff.");
-          message.delete().catch((err) => console.log(err));
-          return;
-        }
-        // If excecuting the command returns true the users message should be removed.
-        const shouldRemoveMessage = command.run(message, messageArr);
-        if (shouldRemoveMessage) {
-          message.delete().catch((err) => console.log(err));
-        }
-      } else {
-        message.reply(
-          `${commandTag} is not a command.\nType \`${
-          Config.CommandPrefix}help\` to see a list of available commands.`);
-        message.delete().catch((err) => console.log(err));
+      // If excecuting the command returns true the users message should be removed.
+      const shouldRemoveMessage = command.run(message, messageArr);
+      if (shouldRemoveMessage) {
+        message.delete().catch((err) => log(err));
       }
+    } else {
+      message.reply(
+        `${commandTag} is not a command.\nType \`${
+        Config.CommandPrefix}help\` to see a list of available commands.`);
+      message.delete().catch((err) => log(err));
     }
   }
 
