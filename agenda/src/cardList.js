3e06e099a6831653f27b395db9f5245b6721366a
Polleps <polleps@gmail.com>
2021-10-07 16:44:38 +0200

    fix date sorting issue

--- agenda/src/cardList.js
+++ agenda/src/cardList.js
@@ -3,39 +3,37 @@ import { repeat } from 'lit-html/directives/repeat';
 import { card } from './card.js';
 import { devider } from './devider.js';
 
-export const cardList = ({ onDateChange, onViewChange}) => {
-  const isNum = x => (+x === +x) && x !== null;
+const isNum = x => (+x === +x) && x !== null;
 
-  return (tournaments, month, year) => {
-    const today = new Date();
-    let lastMonth = undefined;
+export const cardList = ({ onDateChange, onViewChange}) => (tournaments, month, year) => {
+  const today = new Date();
+  let lastMonth = undefined;
 
-    // Sort tournaments by date and only keep upcomming.
-    console.log(JSON.parse(JSON.stringify(tournaments)));
-    const sortedTournaments = tournaments.sort((a, b) => a.startDate - b.startDate)
-                                          .filter(t => t.endDate >= today);
+  // Sort tournaments by date and only keep upcomming.
+  const sortedTournaments = tournaments.filter(t => t.endDate >= today)
+                                      .sort((a, b) => {
+                                        if (!a.startDate) return -1;
+                                        return a.startDate - b.startDate;
+                                      });
+  // Insert deviders between months.
+  const devidedTournaments = sortedTournaments.flatMap((t) => {
+    const startDate = new Date(t.startDate);
+    if (lastMonth !== startDate.getMonth()) {
+      lastMonth = startDate.getMonth();
+      return [
+        { month: lastMonth },
+        t
+      ];
+    }
+    return t;
+  });
 
-    console.log(sortedTournaments);
-    // Insert deviders between months.
-    const devidedTournaments = sortedTournaments.flatMap((t) => {
-      const startDate = new Date(t.startDate);
-      if (lastMonth !== startDate.getMonth()) {
-        lastMonth = startDate.getMonth();
-        return [
-          { month: lastMonth },
-          t
-        ];
-      }
-      return t;
-    });
-    console.log(devidedTournaments);
-    return html`
-      <div class="card-holder">
-        <ul class="card-list">
-          ${repeat(devidedTournaments, (t) => t.title, (t) => isNum(t.month) ? devider(t) : card(t))}
-        </ul>
-      </div>
-    `;
-  }
+  return html`
+    <div class="card-holder">
+      <ul class="card-list">
+        ${repeat(devidedTournaments, (t) => t.title, (t) => isNum(t.month) ? devider(t) : card(t))}
+      </ul>
+    </div>
+  `;
 };
 
